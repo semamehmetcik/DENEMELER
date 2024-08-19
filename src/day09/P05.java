10a047f2d679fdd77bf4bf60d53d691b6e60a577
Sema <semamehmetciksm@gmail.com>
2024-08-20 01:42:43 +0300

    9.gün

---
+++ src/day09/P05.java
@@ -0,0 +1,16 @@
+package day09;
+
+public class P05 {
+    public static void main(String[] args) {
+
+        int input = 50;
+
+        for (int i = input; i >=1 ; i--) {
+            if (i%3==0){
+                System.out.print(i+" - ");
+            }
+
+        }
+
+    }
+}
